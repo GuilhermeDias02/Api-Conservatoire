e0c2a341bef7788c018a0000d004505eeb4f250a
GuilhermeDias02 <guilherme.alex.dias@gmail.com>
2023-05-30 14:21:30 +0200

    first commit

---
+++ db_connect.php
@@ -0,0 +1,7 @@
+<?php
+	$server = "localhost";
+	$username = "root";
+	$password = "";
+	$db = "conservatoire4";
+	$conn = mysqli_connect($server, $username, $password, $db);
+?>
---
+++ produits.php
@@ -0,0 +1,33 @@
+<?php
+	// Connect to database
+	include("db_connect.php");
+	$request_method = $_SERVER["REQUEST_METHOD"];
+
+	function getProducts()
+	{
+		global $conn;
+		$query = "SELECT * FROM login";
+		$response = array();
+		$result = mysqli_query($conn, $query);
+		while($row = mysqli_fetch_array($result))
+		{
+			$response[] = $row;
+		}
+		header('Content-Type: application/json');
+		echo json_encode($response, JSON_PRETTY_PRINT);
+	}
+	
+	switch($request_method)
+	{
+		case 'GET':
+
+			// Retrive Products
+            getProducts();
+
+            break;
+		default:
+			// Invalid Request Method
+			header("HTTP/1.0 405 Method Not Allowed");
+			break;
+	}
+?>
